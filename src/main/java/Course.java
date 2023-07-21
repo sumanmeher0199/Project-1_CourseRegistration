e64a5666339f30f20339d0e6e3e7bd4edabf07c9
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-21 15:50:52 +0530

    added code

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,9 +1,38 @@
 
 public class Course {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String name;
+	int price;
+	String duration;
+	String content;
+	
+	public String getName() {
+		return name;
 	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 
 }
