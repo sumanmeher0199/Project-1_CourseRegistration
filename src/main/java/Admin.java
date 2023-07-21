5d06a491f86732622334a722dc326c9382b1feb8
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-07-21 16:52:51 +0530

    test

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Admin {
 	
@@ -6,12 +7,29 @@ public class Admin {
 	String username;
 	String password;
 	public Admin() {
-		username = "admin";
-		password = "Admin";
+		this.username = "admin";
+		this.password = "Admin";
 	}
 	
 	ArrayList<Professor> professor;
 	ArrayList<Course> course;
 	ArrayList<Student> student;
+	
+	boolean checkUsernamePassword() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Admin Username:");
+		String name = sc.next();
+		System.out.println("Enter Admin Password");
+		String pass = sc.next();
+		
+		if(name.equals(username) && pass.equals(this.password))
+			return true;
+		return false;
+		
+	}
+	
+	void addCourseInList(Course c) {
+		course.add(c);
+	}
 
 }
--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 
 public class Launch {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Course Regestration Website: ");
+		Admin ad = new Admin();
+		boolean isAuth = ad.checkUsernamePassword();
+		if(!isAuth) {
+			System.out.println("Authentication Failed.. \nThanx for visiting us..");
+			System.exit(0);
+		}
+		System.out.println("Authenticated..");
+		
+		//Add course
+		String cont;
+		while(!cont.equals("no")) {
+			Course c1 = new Course();
+//			c1.addCourse();
+			ad.addCourseInList(c1);
+			System.out.println("Do you want to Create more Courses:");
+			String  inp = sc.next();
+			cont = inp;
+		}
+		
+		
 		
 
 	}
