1b698142703d1e1d40a3d167a3920cceb84310c9
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-22 19:28:42 +0530

    changes in student class

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,23 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("******************************************************");
-		System.out.println("*            Certificate of Achievement              *");
-		System.out.println("*                                                    *");
-		System.out.println("*              This is to certify that               *");
-		System.out.println("*                                                    *");
-		System.out.println("*                      [Name]                        *");
-		System.out.println("*                                                    *");
-		System.out.println("*            has successfully completed              *");
-		System.out.println("*                                                    *");
-		System.out.println("*            [Duration] of [Course Name]             *");
-		System.out.println("*                                                    *");
-		System.out.println("*                 under the guidance of              *");
-		System.out.println("*                                                    *");
-		System.out.println("*                    [Professor]                     *");
-		System.out.println("*                                                    *");
-		System.out.println("*                  securing [Grade]                  *");
-		System.out.println("*                                                    *");
-		System.out.println("******************************************************");
+		
 	}
 }
